9dbd0f780305dfb669998d7a929b0890e5f668f7
brafiadi <ahdiatgnr@gmail.com>
2023-07-30 18:44:14 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { MantineProvider } from "@mantine/core";
+import Layout from "./components/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Layout />
+    </MantineProvider>
   );
 }
 
