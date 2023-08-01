9ef4c0972ff71fb0b32962aa82df93368a327fe7
brafiadi <111393545+brafiadi@users.noreply.github.com>
2023-08-01 15:10:06 +0700

    Update CardThree.jsx

--- src/components/dashboard/CardThree.jsx
+++ src/components/dashboard/CardThree.jsx
@@ -1,5 +1,6 @@
 import { DataTable } from "mantine-datatable";
-import { Avatar, Group, Stack, Title, Text } from "@mantine/core";
+import { Avatar, Group, Stack, Title, Text, ThemeIcon } from "@mantine/core";
+import { IconSquareFilled } from "@tabler/icons-react";
 
 const examples = Array.from({ length: 10 }, (_, i) => ({
   id: i + 1,
@@ -22,6 +23,7 @@ export default function TableExample() {
           width: "40%",
           render: ({ avatar, user, position }) => (
             <Group>
+              <ThemeIcon radius="md" color="gray.2" />
               <Avatar src={avatar} radius="md" />
               <Stack spacing={0}>
                 <Title order={5}>{user}</Title>
